ae923c05dd2d92a7e9060e344cce98aade67c71f
Nitin <vfroot@INMHPUUBUNTU14.04LAP42>
2015-11-14 21:02:54 +0530

    Initial cut.

---
+++ demo/README.md
@@ -0,0 +1,2 @@
+ssh -i neu-II.pem ec2-user@ec2-54-229-200-148.eu-west-1.compute.amazonaws.com
+
---
+++ demo/src/main/java/com/example/nitin/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.nitin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    static class FirstController {
+
+        public FirstController() {
+            super();
+        }
+
+        @RequestMapping("/")
+        public String root() {
+            return "timepass";
+        }
+    }
+}
